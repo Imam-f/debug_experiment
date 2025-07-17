dc2349abf82fee757250360765b4bfce5823557b
Imam-f <mohamadimamfirdaus17@gmail.com>
2025-07-17 08:19:08 +0700

    add python interactive

--- script_to_debug.py
+++ script_to_debug.py
@@ -1,5 +1,13 @@
 # script_to_debug.py
 
+# run with python -i script_to_debug.py
+# or ipython -i script_to_debug.py
+# exec(open('script_to_debug.py').read())
+
+# import readline
+# readline.write_history_file('my_history.txt')
+# ~/.ipython/profile_default/history.sqlite
+
 def buggy_function(x):
     y = x + 1
     z = y * 2
