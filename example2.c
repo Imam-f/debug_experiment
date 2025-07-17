a17661605b2e60289305d19279eb48a3511948fb
Imam-f <mohamadimamfirdaus17@gmail.com>
2025-07-17 19:09:18 +0700

    more types

--- example2.c
+++ example2.c
@@ -24,14 +24,33 @@ THERE extract(HELLO x) {
     return (struct there){.x = x.x};
 }
 
+#define typeof_x int
+#define typeof_y char
+
+#define def_hi(val) typedef struct hello##val { int x; } HELLO##val 
+#define hi(val) HELLO##val
+
+def_hi(2);
+def_hi(3);
+
+#define hi_3 hi(3)
+
+hi(3) extracting(hi(2) x) {
+    return (hi_3){.x = x.x};
+}
+
 int main() {
     int x = 50;
-    
+    typeof_x o;
+
     // typedef int HELLO;
     HELLO y = {.x = 3};
+    hi(2) z = {.x = 3};
+    hi(3) p = {.x = 3};
 
     THERE t = {.x = 4};
 
+    hi(2) q = extracting(p);
     HELLO f = extract(x);
 
     // Trigger various breakpoint scenarios
